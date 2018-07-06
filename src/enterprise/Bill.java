d177d4b2638e3a20fa8ce550ffb119ac86069386
Yarik Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2018-07-06 15:15:39 +0300

    implementing bill

--- src/enterprise/Bill.java
+++ src/enterprise/Bill.java
@@ -3,44 +3,64 @@ package enterprise;
 import person.Client;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Bill {
     private final Client client;
     private double totalCost = 0;
     private final ArrayList<Service> billServices;
+    private  Set<Category> billCategories;
+    private Map<Category, Double> CategoriesTotal;
 //    private ArrayList<Service> billServices;
 
 
     public Bill(Client client, ArrayList<Service> billServices) {
         this.client = client;
         this.billServices = billServices;
-        countTotalByServices();
+        setBillCategories();
+        countTotal();
     }
 
     public Bill(Client client, Service billService) {
         this.client = client;
         //this.billServices = new ArrayList<>();
         this.billServices = new ArrayList<>(Collections.singletonList(billService));
-        countTotalByServices();
+        setBillCategories();
+        countTotal();
     }
 
     public Client getClient() {
         return client;
     }
 
+    public Map<Category, Double> getCategoriesTotal() {
+        return CategoriesTotal;
+    }
+
     public void addService(Service service){
         this.billServices.add(service);
-        this.countTotalByServices();
+        setBillCategories();
+        countTotal();
 
     }
 
-    private void countTotalByServices(){
-        this.totalCost = billServices.stream().mapToDouble(Service::getPrice).sum();
+    private void setBillCategories(){
+        this.billCategories = this.billServices.stream()
+                .map(Service::getServiceCategory).collect(Collectors.toSet());
     }
 
-    private void countTotalByCategory(){
+    private void countTotal(){
+        this.totalCost = billServices.stream()
+                .mapToDouble(Service::getPrice)
+                .sum();
+        
+
+    }
+
+    private void countTotalByCategory( Map<Category, Double> categories, ArrayList<Service> services){
 
     }
 
@@ -50,5 +70,23 @@ public class Bill {
 
     public double getTotalCost() {
         return totalCost;
+    }
+
+    @Override
+    public String toString() {
+        String output = "Bill" + "\n" +
+                "client=" + client + "\n";
+
+        for(Map.Entry <Category,Double> entry : getCategoriesTotal().entrySet()){
+            for(Service service : getBillServices()){
+                if(service.getServiceCategory().getName().equals(entry.getKey().getName())){
+                    output += service.getServiceName() + " ------------ " + service.getPrice() + "\n";
+                }
+            }
+            output += "\n" + entry.getKey().getName().toUpperCase() + " ------------ " + entry.getValue().toString() + "\n\n\n";
+        }
+        output += "Total: " + totalCost + "\n";
+
+        return output;
     }
 }
